f8974a76647c31c4cfe8455cf93f25dd5a74f124
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-06-12 22:56:09 +0600

    added jwt

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 //env file
 require('dotenv').config()
+const jwt = require('jsonwebtoken')
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -33,8 +34,41 @@ async function run() {
     const bookingCollection = client.db("flyPoint").collection("bookings")
     const userCollection = client.db("flyPoint").collection("users")
 
+    //jwt related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token })
+    })
+
+    //middlewares 
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers.authorization)
+      if (!req.headers.authorization) {
+          return res.status(401).send({ message: 'unauthorized access' })
+      }
+      const token = req.headers.authorization.split(' ')[1]
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+          if (err) {
+              return res.status(401).send({ message: 'unauthorized access' })
+          }
+          req.decoded = decoded
+          next();
+      })
+      // next()
+    }
+
 
     //users related api
+
+    // get all users data from db
+    app.get('/users', verifyToken, async (req, res) => {
+      console.log(req.headers)
+      const result = await userCollection.find().toArray()
+      res.send(result)
+    })
+
+
     app.post('/users', async (req, res) => {
       const user = req.body
       //checking existing user
@@ -47,11 +81,6 @@ async function run() {
       res.send(result)
     })
 
-    // get all users data from db
-    app.get('/users', async (req, res) => {
-      const result = await userCollection.find().toArray()
-      res.send(result)
-    })
 
 
     // get a user info by email from db
@@ -61,13 +90,53 @@ async function run() {
       res.send(result)
     })
 
+    //make admin
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id
+      const filter = { _id: new ObjectId(id) }
+      const updatedDoc = {
+        $set: {
+          role: 'Admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+    })
+
+    //make delivery man
+    app.patch('/userss/admin/:id', async (req, res) => {
+      const id = req.params.id
+      const filter = { _id: new ObjectId(id) }
+      const updatedDoc = {
+        $set: {
+          role: 'Delivery Man'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+    })
+
 
     //booking related api
+
+    // get all parcel data from db
+    app.get('/bookings', async (req, res) => {
+      const result = await bookingCollection.find().toArray()
+      res.send(result)
+    })
 
     app.get('/booking', async (req, res) => {
       const email = req.query.email
       const query = { email: email }
       const result = await bookingCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    //for statistics
+    app.get('/bookings', async (req, res) => {
+      // const email = req.query.email
+      // const query = { email: email }
+      const result = await bookingCollection.find().toArray()
       res.send(result)
     })
 
